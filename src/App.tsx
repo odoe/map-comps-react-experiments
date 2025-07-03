901434939c3fdebcac4fcba6df33e2465018e497
Rene Rubalcava <rrubalcava@esri.com>
2025-07-03 11:18:55 -0700

    chore: disable rotation via rt-click drag

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,37 @@ import "@arcgis/map-components/components/arcgis-search";
 import "./App.css";
 
 function App() {
-  const handleViewReady = (event: HTMLArcgisMapElement["arcgisViewReadyChange"]) => {
+  const handleViewReady = (
+    event: HTMLArcgisMapElement["arcgisViewReadyChange"],
+  ) => {
     const viewElement = event.target;
     console.log("View is ready:", viewElement);
   };
 
-  const handleViewDoubleClick = (event: HTMLArcgisMapElement["arcgisViewDoubleClick"]) => {
+  const handleViewDoubleClick = (
+    event: HTMLArcgisMapElement["arcgisViewDoubleClick"],
+  ) => {
     console.log("View double-clicked:", event.detail.native);
     event.detail.stopPropagation(); // Prevent the event from propagating further
+  };
+
+  const handleViewDrag = (event: HTMLArcgisMapElement["arcgisViewDrag"]) => {
+    if (event.detail.button === 2) {
+      console.log("right click");
+      event.detail.stopPropagation();
+    }
   };
 
   return (
     <arcgis-map
       itemId="02b37471d5d84cacbebcccd785460e94"
       onarcgisViewReadyChange={handleViewReady}
-      constraints={{
-        rotationEnabled: false,
-      } as __esri.View2DConstraints} // cast to View2DConstraints
+      // constraints={
+      //   {
+      //     rotationEnabled: false,
+      //   } as __esri.View2DConstraints
+      // } // cast to View2DConstraints
+      onarcgisViewDrag={handleViewDrag}
       onarcgisViewDoubleClick={handleViewDoubleClick}
     >
       <arcgis-zoom position="top-left" />
